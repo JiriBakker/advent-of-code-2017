7ee9e609598fff44be3d688fda98324a9518eba1
Jiri Bakker <jbakker@palantir.com>
2018-01-01 20:21:05 +0100

    Implemented Day 12

--- src/challenges/day12.ts
+++ src/challenges/day12.ts
@@ -2,12 +2,73 @@ import { Challenge } from '../util/challenge'
 
 export class Day12 implements Challenge {
 
+    private static parseLinks(input: string[]): any {
+        const links = {};
+        input.forEach(line => {
+            const ids = line.replace(/<-> /g, "")
+                            .replace(/,/g, "")
+                            .split(" ")
+                            .map(id => parseInt(id));
+
+            const addIfDoesNotContain = (obj, id, value) => {
+                if (!(id in obj)) {
+                    obj[id] = value;
+                }
+            }
+
+            addIfDoesNotContain(links, ids[0], {});
+            for (let i = 1; i < ids.length; i++) {
+                addIfDoesNotContain(links[ids[0]], ids[i], true);
+                addIfDoesNotContain(links, ids[i], { });
+                addIfDoesNotContain(links[ids[1]], ids[0], true);
+            }
+        });
+
+        return links;
+    }
+
+    private static collect(id: number, count: number, links: any, groups: any, groupKey: number): number {
+        if (id in groups || !(id in links)) {
+            return count;
+        }
+        groups[id] = groupKey;
+
+        for (let linkId in links[id]) {
+            if (links[id].hasOwnProperty(linkId)) {
+                count = Day12.collect(parseInt(linkId), count, links, groups, groupKey);
+            }
+        }
+
+        return count + 1;
+    }
+
+    private static countUniqueValues(obj: any) {
+        const uniqueValues = {};
+        let count = 0;
+        for (let key in obj) {
+            if (obj.hasOwnProperty(key) && !(obj[key] in uniqueValues)) {
+                uniqueValues[obj[key]] = true;
+                count++;
+            }
+        }
+        return count;
+    }
+
     public a(input: string[]): number {
-        return 0;
+        const links = Day12.parseLinks(input);
+        return Day12.collect(0, 0, links, {}, 0);
     }
 
     public b(input: string[]): number {
-        return 0;
+        const links = Day12.parseLinks(input);
+        const groups = {};
+        for (let id in links) {
+            if (links.hasOwnProperty(id)) {
+                Day12.collect(parseInt(id), 0, links, groups, parseInt(id));
+            }
+        }
+
+        return Day12.countUniqueValues(groups);
     }
 
 }
--- src/test/day12.ts
+++ src/test/day12.ts
@@ -4,23 +4,29 @@ import { expect } from 'chai';
 describe('Day 12', () => {
     const challenge = new Day12();
 
-    // it('a(...)', () => {
-    //     expect(challenge.a([
-    //         "b inc 5 if a > 1",
-    //         "a inc 1 if b < 5",
-    //         "c dec -12 if a >= 1",
-    //         "c inc -20 if c == 12"
-    //     ])).to.equal(1);
-    // });
+    it('a(...)', () => {
+        expect(challenge.a([
+            "0 <-> 2",
+            "1 <-> 1",
+            "2 <-> 0, 3, 4",
+            "3 <-> 2, 4",
+            "4 <-> 2, 3, 6",
+            "5 <-> 6",
+            "6 <-> 4, 5"
+        ])).to.equal(6);
+    });
 
-    // it('b(...)', () => {
-    //     expect(challenge.b([
-    //         "b inc 5 if a > 1",
-    //         "a inc 1 if b < 5",
-    //         "c dec -12 if a >= 1",
-    //         "c inc -20 if c == 12"
-    //     ])).to.equal(12);
-    // });
-    
+    it('b(...)', () => {
+        expect(challenge.b([
+            "0 <-> 2",
+            "1 <-> 1",
+            "2 <-> 0, 3, 4",
+            "3 <-> 2, 4",
+            "4 <-> 2, 3, 6",
+            "5 <-> 6",
+            "6 <-> 4, 5"
+        ])).to.equal(2);
+    });
+
 });
 
