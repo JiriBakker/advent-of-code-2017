c7c4f74cca85496dc249026afe266eb5337f130e
Jiri Bakker <jbakker@palantir.com>
2018-01-02 16:49:17 +0100

    Implemented Day 13

--- src/challenges/day13.ts
+++ src/challenges/day13.ts
@@ -2,12 +2,61 @@ import { Challenge } from '../util/challenge'
 
 export class Day13 implements Challenge {
 
+    private static parseLayers(input: string[]) {
+        const layers = {};
+        let nrOfLayers = 0;
+        input.forEach(line => {
+            const layer = line.replace(/:/g, "").split(" ");
+            const layerNr = parseInt(layer[0]);
+            const depth = parseInt(layer[1]);
+            layers[layerNr] = depth;
+            nrOfLayers = Math.max(nrOfLayers, layerNr);
+        });
+
+        return {
+            layers: layers,
+            nrOfLayers: nrOfLayers
+        };
+    }
+
+    private static computeSeverity(layers: any, nrOfLayers: number, startTime: number) {
+        let severity = -1;
+        for (let time = 0; time <= nrOfLayers; time++) {
+            if (!(time in layers)) {
+                continue;
+            }
+
+            const depth = layers[time];
+            const loopTime = (depth * 2) - 2;
+            const scannerPos = (time + startTime) % loopTime;
+            if (scannerPos == 0) {
+                if (severity == -1) {
+                    severity = 0;
+                }
+                severity += time * depth;
+            }
+        }
+
+        return severity;
+    }
+
     public a(input: string[]): number {
-        return 0;
+        const parsedLayers = Day13.parseLayers(input);
+        return Day13.computeSeverity(parsedLayers.layers, parsedLayers.nrOfLayers, 0);
     }
 
     public b(input: string[]): number {
-        return 0;
+        const maxTries = 10000000;
+        const parsedLayers = Day13.parseLayers(input);
+
+        for (let startTime = 0; startTime < maxTries; startTime++) {
+            const severity = Day13.computeSeverity(parsedLayers.layers, parsedLayers.nrOfLayers, startTime);
+            if (severity < 0) {
+                return startTime;
+            }
+        }
+
+        throw Error(`Unable to pass through firewall safely (maxTries: ${maxTries})`)
     }
 
 }
--- src/test/day13.ts
+++ src/test/day13.ts
@@ -4,23 +4,23 @@ import { expect } from 'chai';
 describe('Day 13', () => {
     const challenge = new Day13();
 
-    // it('a(...)', () => {
-    //     expect(challenge.a([
-    //         "b inc 5 if a > 1",
-    //         "a inc 1 if b < 5",
-    //         "c dec -13 if a >= 1",
-    //         "c inc -20 if c == 13"
-    //     ])).to.equal(1);
-    // });
+    it('a(...)', () => {
+        expect(challenge.a([
+            "0: 3",
+            "1: 2",
+            "4: 4",
+            "6: 4"
+        ])).to.equal(24);
+    });
 
-    // it('b(...)', () => {
-    //     expect(challenge.b([
-    //         "b inc 5 if a > 1",
-    //         "a inc 1 if b < 5",
-    //         "c dec -13 if a >= 1",
-    //         "c inc -20 if c == 13"
-    //     ])).to.equal(13);
-    // });
+    it('b(...)', () => {
+        expect(challenge.b([
+            "0: 3",
+            "1: 2",
+            "4: 4",
+            "6: 4"
+        ])).to.equal(10);
+    });
     
 });
 
