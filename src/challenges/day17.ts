5e9ad143d56e431df8182c045f4bb0f41f4c4270
Jiri Bakker <jbakker@palantir.com>
2018-01-03 14:57:28 +0100

    Implemented Day 17

--- src/challenges/day17.ts
+++ src/challenges/day17.ts
@@ -2,12 +2,39 @@ import { Challenge } from '../util/challenge'
 
 export class Day17 implements Challenge {
 
-    public a(input: string[]): number {
-        return 0;
+    private static spinLock(stepSize: number, nrOfSteps: number, afterValueToFind: number): number {
+        const buffer = [];
+        let pos = 0;
+        let valuePos = -1;
+        let valueAfter = -1;
+        for (let step = 0; step <= nrOfSteps; step++) {
+            pos = step === 0 ? 0 : ((pos + stepSize) % step) + 1;
+            if (valuePos == -1) {
+                buffer.splice(pos, 0, step);
+            }
+
+            if (step == afterValueToFind) {
+                valuePos = pos;
+                valueAfter = buffer[(pos + 1) % buffer.length];
+            }
+            else if (pos <= valuePos) {
+                valuePos++;
+            }
+            else if (pos == valuePos + 1) {
+                valueAfter = step;
+            }
+        }
+        return valueAfter;
     }
 
-    public b(input: string[]): number {
-        return 0;
+    public a(input: string[], nrOfSteps: number = 2017): number {
+        const stepSize = parseInt(input[0]);
+        return Day17.spinLock(stepSize, nrOfSteps, nrOfSteps);
+    }
+
+    public b(input: string[], nrOfSteps: number = 50000000): number {
+        const stepSize = parseInt(input[0]);
+        return Day17.spinLock(stepSize, nrOfSteps, 0);
     }
 
 }
--- src/test/day17.ts
+++ src/test/day17.ts
@@ -4,23 +4,11 @@ import { expect } from 'chai';
 describe('Day 17', () => {
     const challenge = new Day17();
 
-    // it('a(...)', () => {
-    //     expect(challenge.a([
-    //         "b inc 5 if a > 1",
-    //         "a inc 1 if b < 5",
-    //         "c dec -17 if a >= 1",
-    //         "c inc -20 if c == 17"
-    //     ])).to.equal(1);
-    // });
+    it('a(3, 5)', () => expect(challenge.a(["3"], 5)).to.equal(2));
+    
+    it('a(3)', () => expect(challenge.a(["3"])).to.equal(638));
 
-    // it('b(...)', () => {
-    //     expect(challenge.b([
-    //         "b inc 5 if a > 1",
-    //         "a inc 1 if b < 5",
-    //         "c dec -17 if a >= 1",
-    //         "c inc -20 if c == 17"
-    //     ])).to.equal(17);
-    // });
+    it('b(3, 5)', () => expect(challenge.b(["3"], 5)).to.equal(5));
     
 });
 
